8c4cd509957071f8364cc617a279ef38c5ed9efc
harrykuo1 <stu970157@gmail.com>
2022-06-08 09:35:45 +0800

    Modify text and image.py

--- mainBot/reqHandler/image.py
+++ mainBot/reqHandler/image.py
@@ -11,8 +11,6 @@ from mainBot.reqHandler.answer import *
 
 def imageCmd(reqMsg, uid):
     message = []
-    resMsgText = 'Get!'
-    message.append(TextSendMessage(text=resMsgText))
     if(reqMsg[0] == ' '):
         reqMsg = reqMsg[1:]
     reqArr = reqMsg.split()
@@ -35,7 +33,8 @@ def imageCmd(reqMsg, uid):
 def transImg(imgName):
     # Step1. 轉換為HSV
     img = cv2.imread('./static/clientImg/' + str(imgName))
-
+    
+    # Step1. 將圖片從BGR轉換為HSV
     hue_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # Step2. 用顏色分割圖像
@@ -43,18 +42,18 @@ def transImg(imgName):
     high_range = np.array([5, 255, 255])
     th = cv2.inRange(hue_image, low_range, high_range)
 
-    # Step3. 形態學運算，膨脹
-    dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
+    # Step3. 形態學運算，膨脹 使紅色區域變為更加明顯 降低錯誤率
+    dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)), iterations=3)
 
-    # Step4. Hough Circle
-    circles = cv2.HoughCircles(dilated, cv2.HOUGH_GRADIENT, 1, 100, param1=20, param2=20, minRadius=20, maxRadius=10000)
+    # Step4. Hough Circle 提取圖像中的圓形
+    circles = cv2.HoughCircles(dilated, cv2.HOUGH_GRADIENT, 1, 100, param1=20, param2=20, minRadius=20, maxRadius=1000)
 
     circleS = circles.reshape(-1, 3)
     circleS = np.uint16(np.around(circleS))
             
     i = 1
     j = len(circles[0])
-    #銳化用
+    #銳化用filter
     sharpen_filter = np.array([[-1, -1, -1], [-1, 10.5, -1], [-1, -1, -1]])
 
     T = []
@@ -62,14 +61,15 @@ def transImg(imgName):
     # Step5. 繪製    
     for k in circleS:
         cv2.circle(img, (k[0], k[1]), k[2], (0, 255, 0), 2)
-        crop_img = img[k[1]-28:k[1]+28, k[0]-28:k[0]+28]
+        half_circle=int(k[2])-17
+        crop_img = img[k[1]-half_circle:k[1]+half_circle, k[0]-half_circle:k[0]+half_circle]
         #cv2.imshow("cropped", crop_img)
 
         grayed_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY) #灰階
         
         sharped_img = cv2.filter2D(grayed_img, -1, sharpen_filter)#銳化
         
-        kernel = np.ones((2,2), np.uint8)
+        kernel = np.ones((3,3), np.uint8)
         img_denoising = cv2.morphologyEx(sharped_img, cv2.MORPH_OPEN, kernel)#降噪
         
         ret, bin_img = cv2.threshold(img_denoising, 127, 255, cv2.THRESH_BINARY)#二值化
